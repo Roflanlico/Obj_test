fbf6d6741aec2987d2680bc3fdf09d1fc84563f3
Roflanlico <92446877+Roflanlico@users.noreply.github.com>
2025-02-10 04:03:09 +0500

    Update AppartmentPrice.cs

--- AppartmentPrice.cs
+++ AppartmentPrice.cs
@@ -1,5 +1,7 @@
 public class ApartmentPrice
 {
-    public string ApartmentUrl { get; set; }
-    public decimal CurrentPrice { get; set; }
+    public int Id { get; set; } // Идентификатор
+    public string ApartmentUrl { get; set; } // URL квартиры
+    public decimal Price { get; set; } // Текущая цена
+    public DateTime DateChecked { get; set; } // Дата последней проверки цены
 }
